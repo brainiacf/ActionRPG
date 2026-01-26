ad8b801595c29a9cf42cdb960c20c4952ec8cf08
Nikunj <70919626+brainiacf@users.noreply.github.com>
2026-01-26 22:45:27 +0530

    WorldUserWidget

--- Source/ActionRPG/Private/ActionRPGWorldUserWidget.cpp
+++ Source/ActionRPG/Private/ActionRPGWorldUserWidget.cpp
@@ -1,5 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ActionRPGWorldUserWidget.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/SizeBox.h"
+#include "Kismet/GameplayStatics.h"
 
+
+void UActionRPGWorldUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	
+	// 1. Cleanup Check: If the enemy died or the actor was destroyed, remove this health bar
+	if (!IsValid(AttachActor))
+	{
+		RemoveFromParent();
+		UE_LOG(LogTemp,Warning,TEXT("AttachedActor no longer valid,removing Health widget"))
+		return;
+	}
+	
+	FVector2D ScreenPosition;
+	// 2. Projection: Math to convert 3D World Space (Vector) -> 2D Screen Space (Vector2D)
+	if (UGameplayStatics::ProjectWorldToScreen(GetOwningPlayer(),AttachActor->GetActorLocation()+WorldOffset,ScreenPosition))
+	{
+		//dPI Scaling: UI coordinates are not 1:1 with pixels due to resolution scaling.
+		//divide by the viewport scale to get the correct UMG position.
+		float Scale = UWidgetLayoutLibrary::GetViewportScale(this);
+		ScreenPosition /= Scale;
+		// 4. Apply the new position to the container widget
+		if (ParentSizeBox)
+		{
+			ParentSizeBox->SetRenderTranslation(ScreenPosition);
+		}
+	}
+}
--- Source/ActionRPG/Public/ActionRPGWorldUserWidget.h
+++ Source/ActionRPG/Public/ActionRPGWorldUserWidget.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,12 +5,33 @@
 #include "Blueprint/UserWidget.h"
 #include "ActionRPGWorldUserWidget.generated.h"
 
+class USizeBox;
 /**
- * 
+ * this widget class meant to be attached to a specific actor in the world.
+ * it will manually update its screen position in Tick to follow that actor
  */
 UCLASS()
 class ACTIONRPG_API UActionRPGWorldUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	//UI Equivalent of Actor::Tick -> runs Every frame to update position 
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	
+	// A container widget used to move the content around the screen.
+	// meta=(BindWidget) links this pointer to a widget named "ParentSizeBox" in the UMG Designer.
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> ParentSizeBox;
+	
+public:
+	//the actor this widget is floating above
+	UPROPERTY(BlueprintReadOnly,Category=UI)
+	AActor* AttachActor;
+	
+	//offset from the actor's origin(for a character Z = 90 )
+	UPROPERTY(EditDefaultsOnly,Category=UI)
+	FVector WorldOffset;
 	
 };
