b87588b02fbee600c5e2110ca832117bfd19606e
Nikunj <70919626+nem-junk@users.noreply.github.com>
2026-02-18 13:44:07 +0530

    Health is replicated

--- Source/ActionRPG/Private/ActionRPGAttributeComponent.cpp
+++ Source/ActionRPG/Private/ActionRPGAttributeComponent.cpp
@@ -1,31 +1,19 @@
 #include "ActionRPGAttributeComponent.h"
 #include "ActionRPGGameModeBase.h"
+#include "Net/UnrealNetwork.h"
 
 // console variable to manipulate Damage while the game is running, Use .GetValueOnGameThread() to get the var
 static TAutoConsoleVariable<float> CVarDamageMultiplier(TEXT("su.DamageMultiplier"),1.0f,TEXT("MultiplyDamage"),ECVF_Cheat);
 
-//helper function -> to quickly get the Attribute Component from Any actor  
-UActionRPGAttributeComponent* UActionRPGAttributeComponent::GetAttributes(AActor* FromActor)
-{
-	if (FromActor) {return Cast<UActionRPGAttributeComponent>(FromActor->GetComponentByClass(UActionRPGAttributeComponent::StaticClass()));}
-	return nullptr;
-}
 
-bool UActionRPGAttributeComponent::ISActorAlive(AActor* Actor)
-{
-	UActionRPGAttributeComponent*AttributeComponent = GetAttributes(Actor);
-	if (AttributeComponent)
-	{
-		return AttributeComponent->IsAlive();
-	}
-	return false;
-}
 
 UActionRPGAttributeComponent::UActionRPGAttributeComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 	
 	MaxHealth = 100.0f;   Health =MaxHealth;
+	
+	SetIsReplicatedByDefault(true);
 
 }
 bool UActionRPGAttributeComponent::ApplyHealthChange(AActor* InstigatorActor, float DeltaHealth)
@@ -43,7 +31,12 @@ bool UActionRPGAttributeComponent::ApplyHealthChange(AActor* InstigatorActor, fl
 	Health = FMath::Clamp(Health + DeltaHealth,0.0f,MaxHealth);
 	
 	float ActualDelta = Health - OldHealth;
-	OnHealthChange.Broadcast(InstigatorActor,this,Health,ActualDelta);
+//	OnHealthChange.Broadcast(InstigatorActor,this,Health,ActualDelta);
+	if (ActualDelta != 0.0f)
+	{
+		MulticastHealthChanged(InstigatorActor,Health,ActualDelta);	
+	}
+	
 	
 	if (ActualDelta < 0.0f && Health == 0.0f)
 	{
@@ -55,6 +48,7 @@ bool UActionRPGAttributeComponent::ApplyHealthChange(AActor* InstigatorActor, fl
 	}
 	return ActualDelta != 0.0f;
 }
+
 
 bool UActionRPGAttributeComponent::Kill(AActor* InstigatorActor)
 {
@@ -81,4 +75,34 @@ float UActionRPGAttributeComponent::GetHealth() const
 	return Health;
 }
 
+//helper function -> to quickly get the Attribute Component from Any actor  
+UActionRPGAttributeComponent* UActionRPGAttributeComponent::GetAttributes(AActor* FromActor)
+{
+	if (FromActor) {return Cast<UActionRPGAttributeComponent>(FromActor->GetComponentByClass(UActionRPGAttributeComponent::StaticClass()));}
+	return nullptr;
+}
 
+bool UActionRPGAttributeComponent::ISActorAlive(AActor* Actor)
+{
+	UActionRPGAttributeComponent*AttributeComponent = GetAttributes(Actor);
+	if (AttributeComponent)
+	{
+		return AttributeComponent->IsAlive();
+	}
+	return false;
+}
+void UActionRPGAttributeComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UActionRPGAttributeComponent, Health);
+	DOREPLIFETIME(UActionRPGAttributeComponent, MaxHealth);
+	
+	// DOREPLIFETIME_CONDITION -> can be used for bandwidth optimization since, we only need to send the data when it is absolutely needed. basically getting the vars with condition will help in bandwidth send when needed
+	//DOREPLIFETIME_CONDITION(UActionRPGAttributeComponent,MaxHealth,COND_OwnerOnly);
+	
+}
+void UActionRPGAttributeComponent::MulticastHealthChanged_Implementation(AActor* InstigatorActor, float NewHealth,
+	float Delta)
+{
+	OnHealthChange.Broadcast(InstigatorActor,this,NewHealth,Delta);	
+}
--- Source/ActionRPG/Public/ActionRPGAttributeComponent.h
+++ Source/ActionRPG/Public/ActionRPGAttributeComponent.h
@@ -42,10 +42,13 @@ public:
 
 protected:
 	
-	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category=Attributes)
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Replicated,Category=Attributes)
 	float Health;
-	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category=Attributes)
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Replicated,Category=Attributes)
 	float MaxHealth;
+	
+	UFUNCTION(NetMulticast,Reliable)
+	void MulticastHealthChanged(AActor* InstigatorActor, float NewHealth, float Delta);
 
 public:	
 	
